9002feb3915043e343f86122b3277b5d0e720f8e
Falafel <tadgranite@gmail.com>
2023-03-31 21:22:43 -0500

    i broke it

--- timsapp/lib/main.dart
+++ timsapp/lib/main.dart
@@ -1,115 +1,401 @@
+import 'dart:html';
+import 'dart:math';
 import 'package:flutter/material.dart';
 
+Color randomColor() {
+  return Colors.primaries[Random().nextInt(Colors.primaries.length)];
+}
+
 void main() {
-  runApp(const MyApp());
+  runApp(MyApp());
 }
 
 class MyApp extends StatelessWidget {
   const MyApp({super.key});
 
-  // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+        home: Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        title: const Text('Flutter is Fun!'),
       ),
-      home: const MyHomePage(title: 'Flutter Demo Home Page'),
-    );
+      body: ListView(
+          scrollDirection: Axis.horizontal,
+          addAutomaticKeepAlives: false,
+          children: [
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                  flex: 4,
+                  child: Container(
+                    child: const Text('Hi Mom!'),
+                    margin: const EdgeInsets.all(10),
+                    padding: const EdgeInsets.all(10),
+                    color: Colors.amberAccent,
+                    height: 100,
+                    width: 100,
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    child: const Text('Hi Mom!'),
+                    margin: const EdgeInsets.all(10),
+                    padding: const EdgeInsets.all(10),
+                    color: Colors.amberAccent,
+                    height: 100,
+                    width: 100,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                  flex: 4,
+                  child: Container(
+                    child: const Text('Hi Mom!'),
+                    margin: const EdgeInsets.all(10),
+                    padding: const EdgeInsets.all(10),
+                    color: Colors.amberAccent,
+                    height: 100,
+                    width: 100,
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    child: const Text('Hi Mom!'),
+                    margin: const EdgeInsets.all(10),
+                    padding: const EdgeInsets.all(10),
+                    color: Colors.amberAccent,
+                    height: 100,
+                    width: 100,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                  flex: 4,
+                  child: Container(
+                    child: const Text('Hi Mom!'),
+                    margin: const EdgeInsets.all(10),
+                    padding: const EdgeInsets.all(10),
+                    color: Colors.amberAccent,
+                    height: 100,
+                    width: 100,
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    child: const Text('Hi Mom!'),
+                    margin: const EdgeInsets.all(10),
+                    padding: const EdgeInsets.all(10),
+                    color: Colors.amberAccent,
+                    height: 100,
+                    width: 100,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                  flex: 4,
+                  child: Container(
+                    child: const Text('Hi Mom!'),
+                    margin: const EdgeInsets.all(10),
+                    padding: const EdgeInsets.all(10),
+                    color: Colors.amberAccent,
+                    height: 100,
+                    width: 100,
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    child: const Text('Hi Mom!'),
+                    margin: const EdgeInsets.all(10),
+                    padding: const EdgeInsets.all(10),
+                    color: Colors.amberAccent,
+                    height: 100,
+                    width: 100,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                  flex: 4,
+                  child: Container(
+                    child: const Text('Hi Mom!'),
+                    margin: const EdgeInsets.all(10),
+                    padding: const EdgeInsets.all(10),
+                    color: Colors.amberAccent,
+                    height: 100,
+                    width: 100,
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    child: const Text('Hi Mom!'),
+                    margin: const EdgeInsets.all(10),
+                    padding: const EdgeInsets.all(10),
+                    color: Colors.amberAccent,
+                    height: 100,
+                    width: 100,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: [
+                Expanded(
+                  flex: 4,
+                  child: Container(
+                    child: const Text('Hi Mom!'),
+                    margin: const EdgeInsets.all(10),
+                    padding: const EdgeInsets.all(10),
+                    color: Colors.amberAccent,
+                    height: 100,
+                    width: 100,
+                  ),
+                ),
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    child: const Text('Hi Mom!'),
+                    margin: const EdgeInsets.all(10),
+                    padding: const EdgeInsets.all(10),
+                    color: Colors.amberAccent,
+                    height: 100,
+                    width: 100,
+                  ),
+                ),
+              ],
+            ),
+            Stack(
+              children: [
+                Align(
+                  child: Container(
+                    color: Colors.green,
+                    width: 100,
+                    height: 100,
+                  ),
+                  alignment: Alignment.center,
+                ),
+                Align(
+                  child: Icon(Icons.verified),
+                  alignment: Alignment.center,
+                )
+              ],
+            ),
+          ]),
+      floatingActionButton: FloatingActionButton(
+          child: Icon(Icons.add),
+          onPressed: () {
+            print('pressed!');
+          }),
+      bottomNavigationBar: BottomNavigationBar(items: [
+        BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
+        BottomNavigationBarItem(icon: Icon(Icons.business), label: 'Buisness'),
+        BottomNavigationBarItem(icon: Icon(Icons.school), label: 'School'),
+      ]),
+      drawer: Drawer(
+        child: ListView.builder(
+            addAutomaticKeepAlives: false,
+            itemBuilder: (_, index) {
+              return Container(
+                color: randomColor(),
+                width: 500,
+                height: 500,
+              );
+            }),
+      ),
+    ));
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key, required this.title});
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
+class WidgetBoi extends StatefulWidget {
+  const WidgetBoi({Key? key}) : super(key: key);
 
   @override
-  State<MyHomePage> createState() => _MyHomePageState();
+  State<WidgetBoi> createState() => _WidgetBoiState();
 }
 
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class _WidgetBoiState extends State<WidgetBoi> {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
-      ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also a layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
+    return Container(
+      color: randomColor(),
+      width: 500,
+      height: 500,
+    );
+
+    /*ListView(
+      scrollDirection: Axis.vertical,
+      addAutomaticKeepAlives: false,
+      children: [
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 4,
+              child: Container(
+                child: const Text('Hi Mom!'),
+                margin: const EdgeInsets.all(10),
+                padding: const EdgeInsets.all(10),
+                color: Colors.amberAccent,
+                height: 100,
+                width: 100,
+              ),
             ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headline4,
+            Expanded(
+              flex: 2,
+              child: Container(
+                child: const Text('Hi Mom!'),
+                margin: const EdgeInsets.all(10),
+                padding: const EdgeInsets.all(10),
+                color: Colors.amberAccent,
+                height: 100,
+                width: 100,
+              ),
             ),
           ],
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
-    );
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 4,
+              child: Container(
+                child: const Text('Hi Mom!'),
+                margin: const EdgeInsets.all(10),
+                padding: const EdgeInsets.all(10),
+                color: Colors.amberAccent,
+                height: 100,
+                width: 100,
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Container(
+                child: const Text('Hi Mom!'),
+                margin: const EdgeInsets.all(10),
+                padding: const EdgeInsets.all(10),
+                color: Colors.amberAccent,
+                height: 100,
+                width: 100,
+              ),
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 4,
+              child: Container(
+                child: const Text('Hi Mom!'),
+                margin: const EdgeInsets.all(10),
+                padding: const EdgeInsets.all(10),
+                color: Colors.amberAccent,
+                height: 100,
+                width: 100,
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Container(
+                child: const Text('Hi Mom!'),
+                margin: const EdgeInsets.all(10),
+                padding: const EdgeInsets.all(10),
+                color: Colors.amberAccent,
+                height: 100,
+                width: 100,
+              ),
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 4,
+              child: Container(
+                child: const Text('Hi Mom!'),
+                margin: const EdgeInsets.all(10),
+                padding: const EdgeInsets.all(10),
+                color: Colors.amberAccent,
+                height: 100,
+                width: 100,
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Container(
+                child: const Text('Hi Mom!'),
+                margin: const EdgeInsets.all(10),
+                padding: const EdgeInsets.all(10),
+                color: Colors.amberAccent,
+                height: 100,
+                width: 100,
+              ),
+            ),
+          ],
+        ),
+        Row(
+          mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            Expanded(
+              flex: 4,
+              child: Container(
+                child: const Text('Hi Mom!'),
+                margin: const EdgeInsets.all(10),
+                padding: const EdgeInsets.all(10),
+                color: Colors.amberAccent,
+                height: 100,
+                width: 100,
+              ),
+            ),
+            Expanded(
+              flex: 2,
+              child: Container(
+                child: const Text('Hi Mom!'),
+                margin: const EdgeInsets.all(10),
+                padding: const EdgeInsets.all(10),
+                color: Colors.amberAccent,
+                height: 100,
+                width: 100,
+              ),
+            ),
+          ],
+        ),
+      ],
+    );*/
   }
 }
