fd6408d328354cb781cf40406f6b6c2cfde3fb24
Falafel <tadgranite@gmail.com>
2023-04-01 18:15:21 -0500

    new pages

--- timsapp/lib/main.dart
+++ timsapp/lib/main.dart
@@ -1,3 +1,4 @@
+import 'dart:html';
 import 'dart:math';
 import 'package:flutter/material.dart';
 
@@ -207,13 +208,10 @@ class MyApp extends StatelessWidget {
                   alignment: Alignment.center,
                 )
               ],
-            )
+            ),
+            WidgetBoi(),
+            Shark(),
           ]),
-      floatingActionButton: FloatingActionButton(
-          child: Icon(Icons.add),
-          onPressed: () {
-            print('pressed!');
-          }),
       bottomNavigationBar: BottomNavigationBar(items: [
         BottomNavigationBarItem(icon: Icon(Icons.home), label: 'Home'),
         BottomNavigationBarItem(icon: Icon(Icons.business), label: 'Buisness'),
@@ -231,5 +229,88 @@ class MyApp extends StatelessWidget {
             }),
       ),
     ));
+  }
+}
+
+class WidgetBoi extends StatefulWidget {
+  const WidgetBoi({super.key});
+
+  @override
+  State<WidgetBoi> createState() => _WidgetBoiState();
+}
+
+class _WidgetBoiState extends State<WidgetBoi> {
+  int count = 0;
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
+  void dispose() {
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Container(
+          child: Text('$count'),
+          margin: const EdgeInsets.all(10),
+          padding: const EdgeInsets.all(10),
+          color: Colors.amberAccent,
+          height: 100,
+          width: 100,
+        ),
+        FloatingActionButton(
+            child: Icon(Icons.add),
+            onPressed: () {
+              setState(() {
+                count++;
+              });
+            }),
+        FloatingActionButton(
+            child: Icon(Icons.remove),
+            onPressed: () {
+              setState(() {
+                count--;
+              });
+            })
+      ],
+    );
+  }
+}
+
+class PageTwo extends StatelessWidget {
+  const PageTwo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(
+        backgroundColor: Colors.amber,
+        title: const Text('Flutter is Fun!'),
+      ),
+    ));
+  }
+}
+
+class Shark extends StatelessWidget {
+  const Shark({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return ElevatedButton(
+        onPressed: () {
+          Navigator.push(
+              context,
+              MaterialPageRoute(
+                builder: (context) => const PageTwo(),
+              ));
+        },
+        child: Text('About'));
   }
 }
