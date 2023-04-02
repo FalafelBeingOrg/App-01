335cf8c4e328cc24a6a33f780c77acc7dee176d7
Falafel <tadgranite@gmail.com>
2023-04-01 20:51:28 -0500

    idk what changed but i feel like saving

--- timsapp/lib/main.dart
+++ timsapp/lib/main.dart
@@ -1,6 +1,8 @@
 import 'dart:html';
 import 'dart:math';
 import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+import 'firebase_options.dart';
 
 void main() {
   runApp(MyApp());
